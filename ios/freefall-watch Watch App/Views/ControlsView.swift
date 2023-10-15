951abb7b2f16956ef80f1bd6ee8977694e735975
steflw <stefanlw42@gmail.com>
2023-10-15 21:19:07 +0100

    recording btn

--- ios/freefall-watch Watch App/Views/ControlsView.swift
+++ ios/freefall-watch Watch App/Views/ControlsView.swift
@@ -10,9 +10,6 @@ import SwiftUI
 struct ControlsView: View {
   @ObservedObject var jumpManager: JumpManager
     var body: some View {
-      VStack(alignment: .leading) {
-        Text("Record")
-        Button(self.jumpManager.active ? "Stop" : "Start", action: self.jumpManager.active ? self.jumpManager.stop : self.jumpManager.start)
-      }
+        Button(self.jumpManager.active ? "Stop recording" : "Start recording", action: self.jumpManager.active ? self.jumpManager.stop : self.jumpManager.start)
     }
 }
